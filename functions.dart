6e3c9f1a0edc18cdadb565a6dcf0249840208377
farooq13 <farukidris19@gmail.com>
2024-11-08 17:25:20 +0100

    learn function

--- functions.dart
+++ functions.dart
@@ -2,6 +2,17 @@ void main() {
   printNameOnly();
   var name = printName();
   print(name);
+
+  print(printNumberString());
+
+  // Accessing a single var from a func
+  var dif = printNumberString();
+  print(dif.$1);
+
+  String userName = 'Faruk';
+  print(userName);
+
+  newFunc(name: userName, age: 23, greeting: 'Hello');
 }
 
 /*
@@ -15,4 +26,17 @@ void printNameOnly() {
 
 (int, String) printName() {
   return (12, 'Faruk');
+}
+
+(int, String, bool) printNumberString() {
+  return (15, 'Hey', false);
+}
+
+void myName(String name) {
+  print(name);
+}
+
+void newFunc(
+    {required String name, required int age, required String greeting}) {
+  print(age);
 }
